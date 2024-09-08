41fd80af9518ef1da53416b443e419096d2af94d
Graeme Porteous <graeme@rgbp.co.uk>
2024-09-08 10:36:01 +0100

    Setup new Ruby gem project

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Spond Ruby Client";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        spondRuby = pkgs.bundlerEnv {
+          name = "gemset";
+          gemfile = ./Gemfile;
+          lockfile = ./Gemfile.lock;
+          gemset = ./gemset.nix;
+        };
+      in {
+
+      defaultPackage = spondRuby;
+
+      # used by nix shell and nix develop
+      devShell = with pkgs;
+        mkShell {
+          buildInputs = [
+            ruby_3_4
+            bundix
+          ];
+      };
+  });
+}
